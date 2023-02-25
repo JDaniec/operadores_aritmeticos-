994d1288d6a8a0beb87ff2711b4677e6a8a8aff9
Juan Daniel Carreño Rodriguez <126117081+JDaniec@users.noreply.github.com>
2023-02-25 08:14:43 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # operadores_aritmeticos
 programa para implementar los operadores aritméticos
+
+
+# Diseño
+
+## Diagrama de flujo
+
+![Diagrama de flujo](digrama.png "Diagrama de flujo")
---
+++ operadores_aritmeticos.py
@@ -0,0 +1,32 @@
+# Programa para implementar operadores aritmeticos 
+
+print("-----------------------------------------------")
+print("--------------OPERADORES ARITMETICOS----------------")
+print("--------------------------------------------")
+
+# INPUT
+
+X = int(input("digite el valor de x"))
+Y = int(input("digite el valor de y"))
+
+s = X + Y
+r = X - Y
+m = X * Y
+d = X / Y
+mod = X % Y
+de = X // Y
+p = X ** Y
+
+
+#out put
+
+print("----------------------------------")
+print("-----------------RESULTADOS--------------------")
+print("------------------------------------")
+print("Suma: " + str(s))
+print("resta: " + str(r))
+print("multipicacion: " + str(m))
+print("Division: " + str(d))
+print("modulo: " + str(mod))
+print("division entera: " + str(de))
+print("potencia: " + str(p))
